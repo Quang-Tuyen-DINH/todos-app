fef3ad4fd8e2abb520363e5304f1b82ff792b35d
Quang Tuyen DINH <qtuyen43@gmail.com>
2022-12-21 11:03:02 +0100

    Change firebase database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBarqee36KFSHFJTV4S-EtDzlPDO-ruqSQ",
-    authDomain: "pono-todos.firebaseapp.com",
-    databaseURL: "https://pono-todos-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "pono-todos",
-    storageBucket: "pono-todos.appspot.com",
-    messagingSenderId: "394508392163",
-    appId: "1:394508392163:web:bc7bca066fa9e74ec697d6"
+    apiKey: "AIzaSyAgWn_uufakqJezXlapws8bKWaKlz4TpqQ",
+    authDomain: "todos-7fe7a.firebaseapp.com",
+    databaseURL: "https://todos-7fe7a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todos-7fe7a",
+    storageBucket: "todos-7fe7a.appspot.com",
+    messagingSenderId: "798921252726",
+    appId: "1:798921252726:web:e141caaf24cae6df16e457"
   },
   actionCodeSettings: {
     url: 'https://todos-dinh.vercel.app/landing', 
